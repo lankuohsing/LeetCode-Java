b3c645dde620ed68a91eed8fcfa0aeb96046da5e
lankuohsing <lankuohsing@163.com>
2019-04-26 23:19:59 +0800

    change package structures

---
+++ LeetCodeInJava/src/remove_element/copy/RemoveElement.java
@@ -0,0 +1,29 @@
+package remove_element.copy;
+
+import java.util.Arrays;
+
+class Solution1 {
+    public static int removeElement(int[] nums, int val) {
+        int newLength=0;
+        for (int i = 0; i < nums.length; i++) {
+			if (nums[i]!=val) {
+				nums[newLength]=nums[i];
+				newLength++;
+			}
+		}
+        return newLength;
+    }
+}
+public class RemoveElement {
+	public static void main(String[] args) {
+		int[] nums = {0,1,2,2,3,0,4,2};
+		int val=2;
+		int newLength = Solution1.removeElement(nums, val);
+		System.out.println(newLength);
+		int[] nums1=new int[newLength];
+		for (int i = 0; i < newLength; i++) {
+			nums1[i]=nums[i];
+		}
+		System.out.println(Arrays.toString(nums1));
+	}
+}
