f3270b2bd45c3242f241c4dc9e96d574d81b0b3d
lankuohsing <lankuohsing@163.com>
2020-09-28 23:11:40 +0800

    add leetode 77

---
+++ LeetCodeInJava/src/backtrack/seventy_seven/Main.java
@@ -0,0 +1,8 @@
+package backtrack.seventy_seven;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 4, k = 2;
+        System.out.println(new Solution().combine(n, k));
+    }
+}
---
+++ LeetCodeInJava/src/backtrack/seventy_seven/Solution.java
@@ -0,0 +1,27 @@
+package backtrack.seventy_seven;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    List<Integer> oneResult = new ArrayList<Integer>();
+    List<List<Integer>> results = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        dfs(1, n, k);
+        return results;
+    }
+
+    private void dfs(int start, int n, int k) {
+        if (start > n) {
+            if (oneResult.size() == k) {
+                results.add(new ArrayList<Integer>(oneResult));
+            }
+            return;
+        }
+        oneResult.add(start);
+        dfs(start + 1, n, k);
+        oneResult.remove(oneResult.size() - 1);
+        dfs(start + 1, n, k);
+    }
+}
