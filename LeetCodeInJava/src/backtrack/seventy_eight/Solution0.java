47a021da3351dacc0631a85ae501610b976f54c2
lankuohsing <lankuohsing@163.com>
2020-09-29 00:08:29 +0800

    套用模板的78解法，对可能的选择列表进行一一尝试，尝试完后要撤销

--- LeetCodeInJava/src/backtrack/seventy_eight/Solution0.java
+++ LeetCodeInJava/src/backtrack/seventy_eight/Solution0.java
@@ -3,6 +3,18 @@ package backtrack.seventy_eight;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+回溯算法的模板
+result = []
+def backtrack(路径, 选择列表):
+    if 满足结束条件:
+        result.add(路径)
+        return
+    for 选择 in 选择列表:
+        做选择
+        backtrack(路径, 选择列表)
+        撤销选择
+ */
 public class Solution0 {
     List<Integer> oneResult = new ArrayList<Integer>();
     List<List<Integer>> results = new ArrayList<List<Integer>>();
@@ -18,14 +30,14 @@ public class Solution0 {
             results.add(new ArrayList<Integer>(oneResult));
             return;
         }
-        for (Boolean decision : decisions) {
+        for (Boolean decision : decisions) {// 对选择列表的各个选择进行一一尝试
             if (decision) {
                 oneResult.add(nums[start]);// 子集中有当前元素
             }
             dfs(start + 1, nums);
             for (int i = 0; i < oneResult.size(); i++) {
                 if (oneResult.get(i) == nums[start]) {
-                    oneResult.remove(i);
+                    oneResult.remove(i);// 撤销选择
                     i--;
                 }
             }
